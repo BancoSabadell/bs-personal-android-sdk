aa9ce26155a32b502ab96d3ae81f7d4949f713b6
Victor Albertos <me@victoralbertos.com>
2017-02-22 08:59:58 +0100

    Update url pointing to server. Bump version.

--- bs-personal/src/main/java/com/sabadell/bs_personal/internal/net/BsPersonalApi.java
+++ bs-personal/src/main/java/com/sabadell/bs_personal/internal/net/BsPersonalApi.java
@@ -8,7 +8,7 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface BsPersonalApi {
-  String URL_BASE = "http://localhost:8080/personal/api/v1/";
+  String URL_BASE = "http://admin-hackathon.westeurope.cloudapp.azure.com:8080/personal/api/v1/";
 
   @FormUrlEncoded
   @POST("account") Observable<Response<Address>> createSession(@Field("password") String password);
